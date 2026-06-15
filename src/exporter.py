abb40a38f55ac88c86930cc16bb2a5800079f7af
Eeshan0908 <eeshan.sharma.data@gmail.com>
2026-06-15 19:18:00 +0530

    exporter.py done

--- src/exporter.py
+++ src/exporter.py
@@ -8,62 +8,30 @@ class Exporter:
     @staticmethod
     def create_output_folder():
 
-        os.makedirs(
-            "output",
-            exist_ok=True
-        )
+        os.makedirs("output",exist_ok=True)
 
     @staticmethod
-    def save_model(
-        model,
-        file_name="best_model.pkl"
-    ):
+    def save_model(model,file_name="best_model.pkl"):
 
         Exporter.create_output_folder()
 
-        file_path = os.path.join(
-            "output",
-            file_name
-        )
+        file_path = os.path.join("output",file_name)
 
-        with open(
-            file_path,
-            "wb"
-        ) as file:
+        with open(file_path,"wb") as file:
 
-            pickle.dump(
-                model,
-                file
-            )
+            pickle.dump(model,file)
 
-        print(
-            f"\nModel saved: {file_path}"
-        )
+        print(f"\nModel saved: {file_path}")
 
     @staticmethod
-    def save_results(
-        results,
-        file_name="model_results.json"
-    ):
+    def save_results(results,file_name="model_results.json"):
 
         Exporter.create_output_folder()
 
-        file_path = os.path.join(
-            "output",
-            file_name
-        )
+        file_path = os.path.join("output",file_name)
 
-        with open(
-            file_path,
-            "w"
-        ) as file:
+        with open(file_path,"w") as file:
 
-            json.dump(
-                results,
-                file,
-                indent=4
-            )
+            json.dump(results,file,indent=4)
 
-        print(
-            f"\nResults saved: {file_path}"
-        )
+        print(f"\nResults saved: {file_path}")
