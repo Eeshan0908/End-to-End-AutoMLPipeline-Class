d40cd46d64feb853c179db4d36a7d27773d17c78
Eeshan0908 <eeshan.sharma.data@gmail.com>
2026-06-15 19:15:41 +0530

    feature_selection.py done

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -1,125 +1,70 @@
 class FeatureSelector:
 
-    def __init__(
-        self,
-        df,
-        target_column,
-        correlation_threshold=0.05
-    ):
+    def __init__(self,df,target_column,correlation_threshold=0.05):
 
         self.df = df
 
         self.target_column = target_column
 
-        self.correlation_threshold = (
-            correlation_threshold
-        )
+        self.correlation_threshold = (correlation_threshold)
 
     def correlation_features(self):
 
-        corr_matrix = self.df.corr(
-            numeric_only=True
-        )
+        corr_matrix = self.df.corr(numeric_only=True)
 
-        if (
-            self.target_column
-            not in corr_matrix.columns
-        ):
+        if (self.target_column not in corr_matrix.columns):
 
             return []
 
-        target_corr = corr_matrix[
-            self.target_column
-        ].abs()
+        target_corr = corr_matrix[self.target_column].abs()
 
         selected_features = []
 
-        for column, value in (
-            target_corr.items()
-        ):
+        for column, value in (target_corr.items()):
 
-            if (
-                column != self.target_column
-                and value >= self.correlation_threshold
-            ):
+            if (column != self.target_column and value >= self.correlation_threshold):
 
-                selected_features.append(
-                    column
-                )
+                selected_features.append(column)
 
         return selected_features
 
     def covariance_features(self):
 
-        cov_matrix = self.df.cov(
-            numeric_only=True
-        )
+        cov_matrix = self.df.cov(numeric_only=True)
 
-        if (
-            self.target_column
-            not in cov_matrix.columns
-        ):
+        if (self.target_column not in cov_matrix.columns):
 
             return []
 
-        target_cov = cov_matrix[
-            self.target_column
-        ].abs()
+        target_cov = cov_matrix[self.target_column].abs()
 
-        mean_covariance = (
-            target_cov.mean()
-        )
+        mean_covariance = (target_cov.mean())
 
         selected_features = []
 
-        for column, value in (
-            target_cov.items()
-        ):
+        for column, value in (target_cov.items()):
 
-            if (
-                column != self.target_column
-                and value > mean_covariance
-            ):
+            if (column != self.target_column and value > mean_covariance):
 
-                selected_features.append(
-                    column
-                )
+                selected_features.append(column)
 
         return selected_features
 
     def select_features(self):
-        corr_features = set(
-        self.correlation_features()
-    )
+        corr_features = set(self.correlation_features())
 
-        cov_features = set(
-        self.covariance_features()
-    )
+        cov_features = set(self.covariance_features())
 
-        selected_features = list(
-        corr_features.union(
-            cov_features
-        )
-    )
+        selected_features = list(corr_features.union(cov_features))
 
         if len(selected_features) == 0:
 
-            selected_features = [
-
-            col
-
-            for col in self.df.columns
-
-            if col != self.target_column
-        ]
+            selected_features = [col for col in self.df.columns if col != self.target_column]
 
         print(
             "\nNo features selected using "
-            "correlation/covariance."
-        )
+            "correlation/covariance.")
 
-        print(
-            "Using all available features."
-        )
+        print("Using all available features.")
 
         return selected_features
